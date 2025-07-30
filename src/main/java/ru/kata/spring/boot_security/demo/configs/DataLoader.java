95e352f04a92980a8d14170146ca6f9b8a86f410
Nikolai <nspulkin@gmail.com>
2025-07-30 08:39:06 +0300

    Driver and dialect properties deleted. In properties spring.jpa.properties.hibernate.hbm2ddl.auto changed for spring.jpa.hibernate.ddl-auto. Added class, which fill users table (Login: admin - password: admin, login: user - password: user). Added in properties spring.jpa.hibernate.ddl-auto=create-drop. Extra @Column annotations deleted. Added braces for "if".

---
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/DataLoader.java
@@ -0,0 +1,54 @@
+package ru.kata.spring.boot_security.demo.configs;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+import ru.kata.spring.boot_security.demo.models.Role;
+import ru.kata.spring.boot_security.demo.models.User;
+import ru.kata.spring.boot_security.demo.services.UserService;
+import ru.kata.spring.boot_security.demo.repositories.RoleRepository;
+
+import java.util.Set;
+
+@Component
+public class DataLoader implements CommandLineRunner {
+
+    private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
+    private final RoleRepository roleRepository;
+
+    @Autowired
+    public DataLoader(UserService userService, PasswordEncoder passwordEncoder, RoleRepository roleRepository) {
+        this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
+        this.roleRepository = roleRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        // Проверяем, существует ли роль "ROLE_USER"
+        Role userRole = roleRepository.findByName("ROLE_USER")
+                .orElseGet(() -> {
+                    Role role = new Role("ROLE_USER");
+                    return roleRepository.save(role);
+                });
+
+        // Проверяем, существует ли роль "ROLE_ADMIN"
+        Role adminRole = roleRepository.findByName("ROLE_ADMIN")
+                .orElseGet(() -> {
+                    Role role = new Role("ROLE_ADMIN");
+                    return roleRepository.save(role);
+                });
+
+        // Создаем администратора с ролью "ROLE_ADMIN"
+        User admin = new User("admin", passwordEncoder.encode("admin"), 30, "admin@example.com", Set.of(adminRole, userRole));
+        userService.save(admin);
+
+        // Создаем обычного пользователя
+        User user = new User("user", passwordEncoder.encode("user"), 25, "user@example.com", Set.of(userRole));
+        userService.save(user);
+    }
+}
+
+
